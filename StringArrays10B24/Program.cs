d95931a6964a116ab3c82a47a651cac72b071bf2
Shivani Harikrishnan <21SHarikrishnan@heckgrammar.co.uk>
2024-10-23 14:07:13 +0100

    String arrays slayy

--- StringArrays10B24/Program.cs
+++ StringArrays10B24/Program.cs
@@ -16,14 +16,34 @@
             /* use fixed (hard coded) string arrays to test your code. 
              Call your subroutine using your test arrays from Main() routine
              and return the result to Main() */
-            
+
             /* Starter code for your subroutine has been provided*/
-
+            string[] a = {"a", "b",  "c", "d", "e"};
+            string[] b = { "a", "b", "c" };
+            Console.WriteLine(Prefix(a, b));
         }
-        static bool Prefix(string[] wor, string[] pre) 
+        static bool Prefix(string[] word, string[] pre) 
         {
+            int i = 0;
 
-            return;
+            if (pre.Length > word.Length) 
+            {
+                return false;
+            }
+            else
+            {
+                i = 1;
+            }
+
+            while(i  < pre.Length) 
+            {
+                if (word[i] != pre[i])
+                {
+                    return false;
+                }
+                i = i + 1;
+            }
+            return true;
         }
     }
 }
